bf07f85af7266545781295b88a690e2d2cbc5896
Blackroad <VladVell@gmail.com>
2017-01-13 03:40:52 -0700

    continue

--- tests/Card_Reader_test_runner.py
+++ tests/Card_Reader_test_runner.py
@@ -18,25 +18,25 @@ async def Card_Reader_thread():
                 if input('Do you want to run appropriate test(Y/N):  ') == 'Y':
                     pytest.main('test_units_serviceON_withCard.py')
                 else:
-                    pass
+                    continue
 
             elif result[1] == state[1]:
                  result_state = '\nUser Card status: Card not Initialized'
                  result_message = result[3:]
                  print (result_state,'\n'+ "Reason is:" + ' '+(' '.join([i for i in result_message])))
                  if input('Do you want to run appropriate test(Y/N):  ') == 'Y':
-                     pytest.main('test_units_serviceON_withCard.py')
+                     pytest.main('test_units_serviceON_withoutCard.py')
                  else:
-                     pass
+                     continue
 
             elif result[1] == state[2]:
                  result_state = '\nUser Card status: Card not Initialized'
                  result_message = result[3:]
                  print(result_state, '\n' + "Reason is:" + ' ' + (' '.join([i for i in result_message])))
                  if input('Do you want to run appropriate test(Y/N):  ') == 'Y':
-                     pytest.main('test_units_serviceON_withCard.py')
+                     pytest.main('test_units_serviceON_withoutCard.py')
                  else:
-                     pass
+                     continue
 
             elif state[3]:
                  result_state = '\nUser Card status: Card not Initialized'
@@ -45,7 +45,7 @@ async def Card_Reader_thread():
                  if input('Do you want to run appropriate test(Y/N):  ') == 'Y':
                      pytest.main('test_units_serviceON_withoutCardReader.py')
                  else:
-                     pass
+                     continue
 
 
 
