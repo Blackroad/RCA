a300ce45b564f58e4acfb4ffda7c23574b7b2306
Blackroad <VladVell@gmail.com>
2017-01-17 05:32:37 -0700

    added continue

--- tests/test_units_submit_event.py
+++ tests/test_units_submit_event.py
@@ -8,3 +8,14 @@ def test_input_units_REWORK(app):
   #  assert app.unit.unit_status('Disposition Complete')
    # assigned_user = app.unit.assignee_field()
     # assert user_name == assigned_user
+
+def test_input_units_CONTINUE(app):
+    #user_name = app.config['card_data']["user_name"]
+    app.unit.find_units('CONTINUE')
+    assert app.unit.element_presented("//div[@class='buttonPanel']/button[@id='ok_button']")
+    app.unit.submit_unit()
+    assert app.unit.element_presented("//div[@class='buttonPanel']/button[@disabled]")
+    app.unit.open_event_details()
+    # assert app.unit.unit_status('Disposition Complete')
+    # assigned_user = app.unit.assignee_field()
+    # assert user_name == assigned_user
